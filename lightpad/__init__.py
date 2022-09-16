e05a6624b0800ed84ecc24a263f4e1d29b04c093
Tom George Ampiath <74437170+TomAmpiath@users.noreply.github.com>
2022-09-17 01:07:18 +0530

    Made a barebone UI, still need to handle what to do when a file is opened etc..

--- lightpad/__init__.py
+++ lightpad/__init__.py
@@ -1,1 +1,33 @@
-__version__: str = "0.1"
+#  MIT License
+#
+#  Copyright (c) 2022 Tom George Ampiath
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+#
+
+import json
+import os
+from typing import Dict
+
+__version__: str = '0.1'
+
+base_dir: str = os.path.dirname(os.path.relpath(__file__))
+
+with open(os.path.join(base_dir, os.path.pardir, 'meta.json'), 'r') as meta_file:
+    meta: Dict = json.load(meta_file)
